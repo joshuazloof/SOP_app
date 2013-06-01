decb2c59bce2c2802ac82b5e748de28d638c2513
jzloof <joshua.zloof@gmail.com>
2013-06-01 14:38:48 -0700

    First SOP app upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SOPApp::Application.config.secret_token = '6bbd09dcc88c176ce01332e4424086b807ca0143dd7fbeef27eb88526af6b3acdb030bd0f2506e9b3f458b4275e75473c6843b2a9c6c905b0707cea176428fa0'
