5cab172af3ccf260c5af81d73e58380e54132d1c
jzloof <joshua.zloof@gmail.com>
2013-06-05 00:34:07 -0700

    Delete old migration files

--- db/migrate/20130601222523_add_sopid_to_steps.rb
+++
@@ -1,5 +0,0 @@
-class AddSopidToSteps < ActiveRecord::Migration
-  def change
-    add_column :steps, :SOPID, :integer
-  end
-end
--- db/migrate/20130601222917_delete_step.rb
+++
@@ -1,8 +0,0 @@
-class DeleteStep < ActiveRecord::Migration
-  def up
-  		drop_table :steps
-  end
-
-  def down
-  end
-end
